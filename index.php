36a9864417434ad3c6d1eabd6663533d6fe2fc18
alexturpin <alex.turpin@gmail.com>
2015-12-22 13:49:10 -0500

    Basic calendar

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 	require('db.php');
 	require('user.php');
+
+	setlocale(LC_ALL, 'fr-CA');
 ?>
 
 <!DOCTYPE html>
@@ -15,6 +17,60 @@
 	<body>
 		<div class="container">
 			<h1>Horaire officiels de tir</h1>
+
+			<?php
+				try {
+					$date = isset($_GET['date']) ? new DateTime($_GET['date']) : new DateTime();
+				}
+				catch(Exception $e) {
+					$date = new DateTime();
+				}
+
+				echo '<h2>', mb_convert_case(utf8_encode(strftime('%B %Y', $date->getTimestamp())), MB_CASE_TITLE), '</h2>';
+
+				$previousMonth = clone $date;
+				$previousMonth->modify('first day of previous month');
+				$nextMonth = clone $date;
+				$nextMonth->modify('first day of next month');
+			?>
+
+			<p>
+				<a href="index.php?date=<?php echo $previousMonth->format('Y-m'); ?>" class="pull-left">Mois précédent</a>
+				<a href="index.php?date=<?php echo $nextMonth->format('Y-m'); ?>" class="pull-right">Mois suivant</a>
+			</p>
+			<table id="calendar">
+				<tr>
+					<th>Dimanche</th>
+					<th>Lundi</th>
+					<th>Mardi</th>
+					<th>Mercredi</th>
+					<th>Jeudi</th>
+					<th>Vendredi</th>
+					<th>Samedi</th>
+				</tr>
+				<?php
+					$firstDayOfMonth = new DateTime($date->format('Y-m-01'));
+					$lastDayOfMonth = new DateTime($date->format('Y-m-t'));
+
+					$currentDay = 1;
+					$daysInMonth = $lastDayOfMonth->format('j');
+
+					while ($currentDay <= $daysInMonth) {
+						echo '<tr>';
+						for($dayOfWeek = 0; $dayOfWeek < 7; $dayOfWeek++) {
+							echo '<td><div>';
+
+							if (!($currentDay == 1 && $dayOfWeek < $firstDayOfMonth->format('w')) && $currentDay <= $daysInMonth) {
+								echo $currentDay;
+								$currentDay++;
+							}
+
+							echo '</div></td>';
+						}
+						echo '<tr>';
+					}
+				?>
+			</table>
 
 			<?php if ($user) { ?>
 			<p>
