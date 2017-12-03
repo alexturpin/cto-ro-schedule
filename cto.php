96e026c2030692016e7ab79e39f2415ce311599e
alexturpin <alex.turpin@gmail.com>
2017-12-03 15:43:58 -0500

    CTO iframe page

---
+++ cto.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>CTO RO Schedule</title>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=ctoutaouais%40gmail.com&amp;color=%232952A3&amp;src=gpfu511hburcrkl09cmk1atbo44dtqko%40import.calendar.google.com&amp;color=%23865A5A&amp;ctz=America%2FToronto" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
+		<p><a href="login.php" target="_parent">Accès officiels de tir</a></p>
+	</body>
+</html>
